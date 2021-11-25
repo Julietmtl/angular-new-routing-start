e0fb9aa89419e05fd5a10e28002f4895b806f404
Juliet <li_juliet@hotmail.com>
2021-11-25 13:45:37 -0500

    resolve dynamic data with resolve guard 152

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { ServerComponent } from "./servers/server/server.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuard } from "./auth-guard.service";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { ErrorPageComponent } from "./error-page/error-page.component";
+import { ServerResolver } from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -24,7 +26,11 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuard],
     component: ServersComponent,
     children: [
-      { path: ":id", component: ServerComponent },
+      {
+        path: ":id",
+        component: ServerComponent,
+        resolve: { server: ServerResolver },
+      },
       {
         path: ":id/edit",
         component: EditServerComponent,
@@ -33,7 +39,11 @@ const appRoutes: Routes = [
     ],
   },
   // { path: "not-found", component: PageNotFoundComponent },
-  { path: "not-found", component: ErrorPageComponent },
+  {
+    path: "not-found",
+    component: ErrorPageComponent,
+    data: { message: "Page not found!" },
+  },
   { path: "**", redirectTo: "/not-found" }, //catches all the other paths not found, this has to be the last route in the appRoutes
 ];
 
